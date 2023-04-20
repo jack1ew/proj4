634fd54ca50f424615ec2b4d469f5d8b0c032cfc
jack1ew <jackiew@email.sc.edu>
2023-04-20 15:44:57 -0400

    Update

--- mmap_fstream.cc
+++ mmap_fstream.cc
@@ -114,8 +114,6 @@ mem_map::fstream& mem_map::fstream::put(char c) {
   if (cursor == size_) {
     size_++;
     ftruncate(fd, size_);
-    char* temp = static_cast<char*>(mremap(file_, size_ - 1, size_, MREMAP_MAYMOVE));
-    file_ = temp;
   }
   file_[cursor] = c;
   cursor++;
